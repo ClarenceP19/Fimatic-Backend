36981658b911a7bea3f62e3b4109cfba22381f80
clarence pang <zwpang.2016@smu.edu.sg>
2019-08-17 15:31:08 +0800

    Set up Spring Boot Application

---
+++ src/main/java/com/Fimatic/fimatic/FimaticApplication.java
@@ -0,0 +1,13 @@
+package com.Fimatic.fimatic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FimaticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FimaticApplication.class, args);
+	}
+
+}
